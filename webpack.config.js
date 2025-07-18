8edf8e1bf636fb8b055c102411886b8030d0dc77
charlie-daniels <charlied030401@gmail.com>
2025-07-18 21:35:56 +0100

    add: webpack integration

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development', 
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/assets/icons/favicon.ico',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+    hot: true,
+    watchFiles: ['src/**/*'],
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.js'],
+  },
+};
