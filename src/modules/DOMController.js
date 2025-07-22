9f5519aa9e75dbb7f4078d9c924530dc335cb9e3
charlie-daniels <charlied030401@gmail.com>
2025-07-22 22:51:04 +0100

    add: basic storage functionality

--- src/modules/DOMController.js
+++ src/modules/DOMController.js
@@ -1,4 +1,11 @@
 import TimeManager from './TimeManager.js';
+import Storage from './Storage.js';
+
+/* TODO: 
+  Move imports to index.js
+  Requires module class restructure
+  Must be called from index.js
+*/
 
 const createActivityElement = () => {
     const newElem = document.createElement('div');
@@ -10,35 +17,40 @@ const createActivityElement = () => {
     lowerDiv.className = 'lower';
 
     const project = Object.assign(document.createElement('input'), {
-      className: 'project',
+      className: 'project value',
       placeholder: '0000',
       maxLength: 4,
     });
 
     const task = Object.assign(document.createElement('input'), {
-      className: 'task',
+      className: 'task value',
       placeholder: 'task...',
     });
 
     const startTime = Object.assign(document.createElement('p'), {
-      className: 'start-time',
+      className: 'start-time status-complete-start value',
       textContent: TimeManager.getTimeHrsMins(),
     });
 
     const endTime = Object.assign(document.createElement('p'), {
-      className: 'end-time unset', 
-      textContent: 'end task',
+      className: 'end-time status-complete-end unset value', 
     });
 
     const elapsedTime = Object.assign(document.createElement('p'), {
-      className: 'elapsed-time', 
+      className: 'elapsed-time status-complete value', 
+    });
+
+    const btnEndTask = Object.assign(document.createElement('button'), {
+      className: 'end-task status-incomplete unset', 
+      textContent: 'end task',
     });
 
     /* complete task, set time, set elapsed time */
 
-    endTime.addEventListener('click', () => {
+    btnEndTask.addEventListener('click', () => {
       endTime.classList.remove('unset');
       newElem.classList.remove('unset');
+      btnEndTask.classList.add('hidden');
 
       endTime.textContent = TimeManager.getTimeHrsMins();
 
@@ -50,7 +62,7 @@ const createActivityElement = () => {
     }, { once: true });
 
     upperDiv.append(project, task);
-    lowerDiv.append(startTime, endTime, elapsedTime);
+    lowerDiv.append(startTime, endTime, elapsedTime, btnEndTask);
     
     newElem.append(upperDiv, lowerDiv);
     return newElem;
@@ -68,10 +80,26 @@ const focusNewActivity = () => {
 
 /* Listeners */
 
-/* Create new element and focus the first input of it */
+/* Sorts activities into array of objects with key value pairs. Assumes first class is key */
+const initGetAllActivites = () => {
+  const getActivitiesValues = () => {
+    return [...document.querySelectorAll('.activity')].map((activity) => {
+      return [...activity.querySelectorAll('.value')].reduce((obj, next) => {
+        return {...obj, [next.classList[0]]: next.textContent || next.value} ;
+      }, {});
+    });
+  };
 
-const detectNewActivity = () => {
-  const btnNewActivity = document.querySelector('.new-activity');
+  const btnStoreActivities = document.querySelector('#store-activities');
+  btnStoreActivities.addEventListener('click', () => {
+    console.log(getActivitiesValues());
+/*     Storage.storeActivities(getActivitiesValues()); */
+  });
+};
+
+/* Create new element and focus the first input of it */
+const initDetectNewActivity = () => {
+  const btnNewActivity = document.querySelector('#new-activity');
   btnNewActivity.addEventListener('click', () => {
     appendActivity();
     focusNewActivity();
@@ -79,5 +107,6 @@ const detectNewActivity = () => {
 };
 
 const addListeners = (() => {
-  detectNewActivity();
+  initGetAllActivites();
+  initDetectNewActivity();
 })();
